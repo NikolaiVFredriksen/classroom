3946b46a7ffce24d5b122625552924851403c81a
Nikolai V. Fredriksen <cooprix1@gmail.com>
2026-08-25 20:26:12 +0200

    Added sidebar and updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
 import {
   Refine,
-  GitHubBanner,
-  WelcomePage,
-  Authenticated,
 } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { BrowserRouter, Route, Routes, Outlet } from "react-router";
 import routerProvider, {
-  NavigateToResource,
-  CatchAllNavigate,
   UnsavedChangesNotifier,
   DocumentTitleHandler,
 } from "@refinedev/react-router";
 import { dataProvider } from "./providers/data";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { ForgotPassword } from "./pages/forgot-password";
-import { ErrorComponent } from "./components/refine-ui/layout/error-component";
-import { Layout } from "./components/refine-ui/layout/layout";
-import { Header } from "./components/refine-ui/layout/header";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import "./App.css";
+import Dashboard from "@/pages/dashboard.tsx";
+import {Home} from "lucide-react";
+import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -42,16 +33,26 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "o1MERR-26XeKN-TgXcnT",
               }}
+              resources={[
+                {
+                  name: 'dashboard', list: '/', meta: {label: 'Home', icon: <Home />}
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                  <Route index element={<Dashboard />} />
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
               <UnsavedChangesNotifier />
               <DocumentTitleHandler />
             </Refine>
-            <DevtoolsPanel />
           </DevtoolsProvider>
         </ThemeProvider>
       </RefineKbarProvider>
